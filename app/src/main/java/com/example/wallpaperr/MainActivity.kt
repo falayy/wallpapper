e7d25d70a819e5bb36260363352195490961b66b
iyanuoluwa <corneliuze>
2019-09-11 14:11:07 +0100

    first commit

---
+++ app/src/main/java/com/example/wallpaperr/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wallpaperr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
